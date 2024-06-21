a216cda236c03dd54166dec64073d2702a0d72c4
Naveen-g09 <naveengummella.rg@gmail.com>
2024-06-21 20:40:52 +0530

    init

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -8,7 +8,7 @@ const io = new Server({
 
 io.on("connection", (socket) => {
     console.log("New connection", socket.id);
-
+    //TODO: Add event listeners here, this would hold a conversation between the client and the server and all the residents of the society, should be able to see the conversation. save them in a database and retrieve them when needed. and flush after a week or so.
     socket.on('disconnect', () => {
         console.log("User disconnected", socket.id);
     });
