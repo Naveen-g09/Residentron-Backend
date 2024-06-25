fb1fa7248a363dd2896aeb1ba975a6da8d2f76d4
Naveen-g09 <naveengummella.rg@gmail.com>
2024-06-25 20:21:42 +0530

    basic websocket

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -1,20 +1,85 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
 import { Server } from "socket.io";
 
-const io = new Server({
-    cors: {
-    origin: ["*"]
-    }
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
+
+const PORT = 4000;
+
+function createUniqueId() {
+  return Math.random().toString(20).substring(2, 10);
+}
+
+interface ChatGroup {
+  id: number;
+  currentGroupName: string;
+  messages: any[]; 
+}
+
+let chatgroups: ChatGroup[] = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 io.on("connection", (socket) => {
-    console.log("New connection", socket.id);
-    //TODO: Add event listeners here, this would hold a conversation between the client and the server and all the residents of the society, should be able to see the conversation. save them in a database and retrieve them when needed. and flush after a week or so.
-    socket.on('disconnect', () => {
-        console.log("User disconnected", socket.id);
-    });
+  console.log(`${socket.id} user is just connected`);
 
+  socket.on("getAllGroups", () => {
+    socket.emit("groupList", chatgroups);
+  });
+
+  socket.on("createNewGroup", (currentGroupName: string) => {
+    console.log(currentGroupName);
+    chatgroups.unshift({
+      id: chatgroups.length + 1,
+      currentGroupName,
+      messages: [],
+    });
+    socket.emit("groupList", chatgroups);
+  });
+
+  socket.on("findGroup", (id: number) => {
+    const filteredGroup = chatgroups.filter((item) => item.id === id);
+    if (filteredGroup.length > 0) {
+      socket.emit("foundGroup", filteredGroup[0].messages);
+    }
+  });
+
+  socket.on("newChatMessage", (data: any) => {
+    const { currentChatMesage, groupIdentifier, currentUser, timeData } = data;
+    const filteredGroup = chatgroups.filter(
+      (item) => item.id === groupIdentifier
+    );
+    if (filteredGroup.length > 0) {
+      const newMessage = {
+        id: createUniqueId(),
+        text: currentChatMesage,
+        currentUser,
+        time: `${timeData.hr}:${timeData.mins}`,
+      };
+  
+      socket
+        .to(filteredGroup[0].currentGroupName)
+        .emit("groupMessage", newMessage);
+      filteredGroup[0].messages.push(newMessage);
+      socket.emit("groupList", chatgroups);
+      socket.emit("foundGroup", filteredGroup[0].messages);
+    }
+  });
 });
 
-console.log("Server running on port 3000");
-io.listen(3000);
+app.get("/api", (req, res) => {
+  res.json(chatgroups);
+});
 
+server.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
